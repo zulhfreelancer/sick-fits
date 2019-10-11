9317b987494c72cbf068229a4fbf7af7d4108c8a
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2019-10-12 00:27:56 +0800

    Global styling and typography with styled components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -24,6 +24,34 @@ const Inner = styled.div`
     padding: 2rem;
 `;
 
+injectGlobal`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+        overflow-x: hidden;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5px;
+        line-height: 2;
+        font-family: 'radnika_next';
+    }
+    a {
+        text-decoration: none;
+        color: ${theme.black};
+    }
+`;
+
 class Page extends Component {
     render() {
         return (
